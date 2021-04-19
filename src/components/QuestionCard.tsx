5f615559c091e4accc524981e2c63d4225b14674
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-04-19 13:21:42 -0700

    Question card

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { NamedTupleMember } from 'typescript';
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNr: number;
+    totalQuestions: number;
+}
+
+
+
+const QuestionCard: React.FC<Props> = ({ 
+        question, 
+        answers, 
+        callback, 
+        userAnswer, 
+        questionNr, 
+        totalQuestions
+    }) => (
+      (
+        <div> 
+           <p className="number">
+               Question: {questionNr} / {totalQuestions}
+           </p>
+
+           <p dangerouslySetInnerHTML={{ __html: question }}>
+              <div>
+                  {answers.map(answer =>(
+                      <div>
+                          <button disabled={userAnswer} onClick={callback}> 
+                              <span dangerouslySetInnerHTML={{__html: answer}} /> 
+                          </button>
+                      </div>
+                  ))}
+              </div>
+           </p>
+        </div>
+      )
+      
+      );
+
+export default QuestionCard;
